c7834a872a3f21b370211ccb66f2fcca7ba2a81d
Justin <jxm262@gmail.com>
2014-12-11 09:49:59 -0500

    Adding   -grunt   -istanbul   -sample mocha test   -travis yml
    Removing 
      -old express files/directories
    
    Changing 
      -start script in package.json to allow command 'npm test' , will run
    mocha test suite

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+/*global module:false*/
+module.exports = function (grunt) {
+
+	grunt.initConfig({
+		jshint : {
+			src : ['Gruntfile.js', 'lib/**/*.js', 'src/config.js', 'test/node/**/*.js'],
+			options : {
+				curly : true,
+				eqeqeq : true,
+				immed : true,
+				latedef : true,
+				newcap : true,
+				noarg : true,
+				sub : true,
+				undef : true,
+				boss : true,
+				eqnull : true,
+				browser : true,
+				globals : {
+					require : true,
+					define : true,
+					requirejs : true,
+					describe : true,
+					expect : true,
+					it : true
+				}
+			}
+		},
+		watch : {
+			files : '<%= jshint.src %>',
+			tasks : ['jshint']
+		},
+		mocha : {
+			all : ["test/app_test.js"]
+		}
+	});
+
+	// Load JSHint task
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-mocha');
+
+	// Default task.
+	grunt.registerTask('default', ['jshint', 'mocha']);
+
+};
