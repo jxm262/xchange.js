4cbf892ec2c9ea2d028d1225475335cbf91754ec
Justin <jxm262@gmail.com>
2014-12-29 13:18:56 -0500

    adding api module for map of apis and rest urls

---
+++ apis/apis.js
@@ -0,0 +1,9 @@
+/**
+ * List of exchanges and corresponding REST url to retrieve current price
+ */
+
+module.exports = {
+	"bitstamp" : "https://www.bitstamp.net/api/ticker/"
+	,"coinbase" : "https://api.coinbase.com/v1/prices/spot_rate"
+	,"bitfinex" : "https://api.bitfinex.com/v1/pubticker/btcusd"
+}
---
+++ test/apis.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var apis = require('../apis/apis.js')
+	,assert = require("assert")
+	,chai = require('chai')
+	,sinon = require('sinon')
+	,should = chai.should()
+	,expect = chai.expect;
+
+describe('apis', function(){
+	it('should contain map of exchange names and corresponding REST api for the spot price', function(){
+		apis.should.have.property("bitstamp", "https://www.bitstamp.net/api/ticker/");
+		apis.should.have.property("coinbase", "https://api.coinbase.com/v1/prices/spot_rate");
+		apis.should.have.property("bitfinex", "https://api.bitfinex.com/v1/pubticker/btcusd");
+	});
+});
