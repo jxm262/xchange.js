ca281bc0dca6ce5b256e1d383740b190c886ead8
Justin <jxm262@gmail.com>
2014-12-29 10:56:49 -0500

    hell i can't remember what i added.  was playing around for too long without committing.  Soo... I'll use this as the new starting point :)

--- test/xchange.test.js
+++ test/xchange.test.js
@@ -1,21 +1,24 @@
 /**
- * Todo: Note, these are actually integration tests.  Need switched to true unit tests after I read up on 
- * mocking and stubbing nested asynchronous calls.  
- * 	
- * 	my so question - http://stackoverflow.com/questions/27462799/sinon-how-to-stub-nested-function
+ * Todo: Note, these are actually integration tests. Need switched to true unit
+ * tests after I read up on mocking and stubbing nested asynchronous calls.
  * 
- * Also need to refactor how to validate json schema
- * 	To follow up and read - 	http://chaijs.com/plugins/chai-json-schema
+ * my so question -
+ * http://stackoverflow.com/questions/27462799/sinon-how-to-stub-nested-function
+ * 
+ * Also need to refactor how to validate json schema To follow up and read -
+ * http://chaijs.com/plugins/chai-json-schema
  */
 
 'use strict';
 
-var xchange = require('../xchange.js')
+var xchange = require('../lib/xchange.js')
 	,assert = require("assert")
 	,chai = require('chai')
 	,sinon = require('sinon')
 	,should = chai.should()
 	,expect = chai.expect;
+
+var request = require('request');
 
 describe('xchange library', function() {
 	it('should have a bistamp object', function() {
@@ -50,21 +53,23 @@ describe('bitstamp object', function() {
 describe('coinbase object', function() {
 	var coinbase = xchange.coinbase;
 	
+	before(function() {
+		sinon.stub(request, 'get').yields(null, JSON.stringify({price: '100 USD'}));
+	});
+
 	it('should have a getTicker function', function() {
 		assert.equal(typeof coinbase.getTicker, 'function');
 	});
 	
-	it('coinbase ticker should return 200, contain coinbase ticker json', function(done) {
-		coinbase.getTicker(function(error, response, body){
-			var ticker = JSON.parse(body);
-			
-			response.statusCode.should.equal(200);
-			ticker.should.have.property("amount");
-			ticker.should.have.property("currency");
+	it('request.get() function should be called and the callback invoked', function(){
+		var callback = sinon.spy();
 
-			done();
-		});
+		coinbase.getTicker(callback);
+		
+		request.get.should.have.been.called;
+		callback.should.have.been.called;
 	});
+	
 });
 
 describe('bitfinex object', function() {
