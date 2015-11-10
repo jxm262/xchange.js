ab77eb8e5a66c1f038219c53547f1e9dac500ef1
Justin Maat <justin.maat@wendys.com>
2015-11-09 23:01:20 -0500

    adding test coverage to mocha task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp   = require('gulp');
+var mocha = require('gulp-mocha');
 var jshint = require('gulp-jshint');
 var istanbul = require('gulp-istanbul');
 
@@ -8,4 +9,19 @@ gulp.task('lint', function() {
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'))
         .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('test', function () {
+    return gulp.src('./apis/**/**.js')
+        // Right there
+        .pipe(istanbul({includeUntested: true}))
+        .on('finish', function () {
+            gulp.src('./test/**/**.js')
+                .pipe(mocha({reporter: 'spec'}))
+                .pipe(istanbul.writeReports({
+                    dir: './reports/unit-test-coverage',
+                    reporters: [ 'lcov' ],
+                    reportOpts: { dir: './reports/unit-test-coverage'}
+                }));
+        });
 });
