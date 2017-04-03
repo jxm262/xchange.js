919d135178c804b6d61d3c78c880a7c30d1156d7
Justin <jxm262@gmail.com>
2017-04-02 21:46:49 -0400

    adding kraken assets api

--- test/exchanges/kraken.test.js
+++ test/exchanges/kraken.test.js
@@ -15,6 +15,150 @@ const serverTimeResp = {
     }
 };
 
+const assetsResp = {
+    "error": [],
+    "result": {
+        "KFEE": {
+            "aclass": "currency",
+            "altname": "FEE",
+            "decimals": 2,
+            "display_decimals": 2
+        },
+        "USDT": {
+            "aclass": "currency",
+            "altname": "USDT",
+            "decimals": 8,
+            "display_decimals": 4
+        },
+        "XDAO": {
+            "aclass": "currency",
+            "altname": "DAO",
+            "decimals": 10,
+            "display_decimals": 3
+        },
+        "XETC": {
+            "aclass": "currency",
+            "altname": "ETC",
+            "decimals": 10,
+            "display_decimals": 5
+        },
+        "XETH": {
+            "aclass": "currency",
+            "altname": "ETH",
+            "decimals": 10,
+            "display_decimals": 5
+        },
+        "XICN": {
+            "aclass": "currency",
+            "altname": "ICN",
+            "decimals": 10,
+            "display_decimals": 5
+        },
+        "XLTC": {
+            "aclass": "currency",
+            "altname": "LTC",
+            "decimals": 10,
+            "display_decimals": 5
+        },
+        "XMLN": {
+            "aclass": "currency",
+            "altname": "MLN",
+            "decimals": 10,
+            "display_decimals": 5
+        },
+        "XNMC": {
+            "aclass": "currency",
+            "altname": "NMC",
+            "decimals": 10,
+            "display_decimals": 5
+        },
+        "XREP": {
+            "aclass": "currency",
+            "altname": "REP",
+            "decimals": 10,
+            "display_decimals": 5
+        },
+        "XXBT": {
+            "aclass": "currency",
+            "altname": "XBT",
+            "decimals": 10,
+            "display_decimals": 5
+        },
+        "XXDG": {
+            "aclass": "currency",
+            "altname": "XDG",
+            "decimals": 8,
+            "display_decimals": 2
+        },
+        "XXLM": {
+            "aclass": "currency",
+            "altname": "XLM",
+            "decimals": 8,
+            "display_decimals": 5
+        },
+        "XXMR": {
+            "aclass": "currency",
+            "altname": "XMR",
+            "decimals": 10,
+            "display_decimals": 5
+        },
+        "XXRP": {
+            "aclass": "currency",
+            "altname": "XRP",
+            "decimals": 8,
+            "display_decimals": 5
+        },
+        "XXVN": {
+            "aclass": "currency",
+            "altname": "XVN",
+            "decimals": 4,
+            "display_decimals": 2
+        },
+        "XZEC": {
+            "aclass": "currency",
+            "altname": "ZEC",
+            "decimals": 10,
+            "display_decimals": 5
+        },
+        "ZCAD": {
+            "aclass": "currency",
+            "altname": "CAD",
+            "decimals": 4,
+            "display_decimals": 2
+        },
+        "ZEUR": {
+            "aclass": "currency",
+            "altname": "EUR",
+            "decimals": 4,
+            "display_decimals": 2
+        },
+        "ZGBP": {
+            "aclass": "currency",
+            "altname": "GBP",
+            "decimals": 4,
+            "display_decimals": 2
+        },
+        "ZJPY": {
+            "aclass": "currency",
+            "altname": "JPY",
+            "decimals": 2,
+            "display_decimals": 0
+        },
+        "ZKRW": {
+            "aclass": "currency",
+            "altname": "KRW",
+            "decimals": 2,
+            "display_decimals": 0
+        },
+        "ZUSD": {
+            "aclass": "currency",
+            "altname": "USD",
+            "decimals": 4,
+            "display_decimals": 2
+        }
+    }
+};
+
 const errMsg = {msg: "test-error"};
 
 
@@ -27,6 +171,16 @@ nock('https://api.kraken.com')
 
 nock('https://api.kraken.com')
     .get('/0/public/Time')
+    .twice()
+    .replyWithError(errMsg);
+
+nock('https://api.kraken.com')
+    .get('/0/public/Assets')
+    .twice()
+    .reply(200, assetsResp);
+
+nock('https://api.kraken.com')
+    .get('/0/public/Assets')
     .twice()
     .replyWithError(errMsg);
 
@@ -76,6 +230,42 @@ describe.only('kraken', function () {
 
             it('retrieves error using promise', function (done) {
                 kraken.serverTime().then(
+                    success,
+                    failure(done)
+                );
+            });
+        });
+
+    });
+
+    describe('assets', function () {
+
+        context('success call', function () {
+            it('retrieves asset info using cb', function (done) {
+                kraken.assets(function (err, resp) {
+                    resp.should.deep.equal(assetsResp);
+                    done();
+                });
+            });
+
+            it('retrieves asset info using using promise', function (done) {
+                kraken.assets().then(
+                    success(assetsResp, done),
+                    failure
+                );
+            });
+        });
+
+        context('failure call', function () {
+            it('retrieves error using cb', function (done) {
+                kraken.assets(function (err, resp) {
+                    err.should.deep.equal(errMsg)
+                    done();
+                });
+            });
+
+            it('retrieves error using promise', function (done) {
+                kraken.assets().then(
                     success,
                     failure(done)
                 );
