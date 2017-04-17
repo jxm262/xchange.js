0553fddbae338b6d2a5490151f47db35ab182f2d
Justin <jxm262@gmail.com>
2017-04-17 10:32:00 -0400

    adding logic to segment urls with user data

--- test/parsers.test.js
+++ test/parsers.test.js
@@ -1,10 +1,27 @@
 import nock from 'nock';
 import chai from 'chai';
 import sinon from 'sinon';
-import { parsefilterToQueryParams } from '../lib/parsers'
+import _ from 'lodash';
+import { parsefilterToQueryParams, dataToSegmentedUrl } from '../lib/parsers'
 
 
 describe('parsers', function () {
+
+
+    describe('dataToSegmentedUrl', function() {
+        it.only('converts input data {data: "value"} and url /endpoint/data to new url /endpoint/value', function () {
+            const url = '/hello/<%=segment1%>/world/<%=segment2%>/test';
+            const templated = _.template(url);
+            const inputData = {
+                segment1: "hola",
+                segment2: "mundo"
+            };
+
+            const segmentedUrl = templated(inputData);
+            segmentedUrl.should.equal('/hello/hola/world/mundo/test');
+        });
+    });
+
     describe('parsefilterToQueryParams', function () {
         it('parses array to csv', function () {
             const body = {
