d2a122ec585d5d2a0133a80ac5ee0b5572aeb03a
Justin <jxm262@gmail.com>
2017-04-17 15:28:56 -0400

    adding bitstamp apis hourlyTicker and orderBook

--- test/exchanges/bitstamp.test.js
+++ test/exchanges/bitstamp.test.js
@@ -18,6 +18,26 @@ nock(rootUrl)
     .twice()
     .replyWithError(testErrMsg);
 
+nock(rootUrl)
+    .get('/ticker_hour/btcusd')
+    .twice()
+    .reply(200, apis.unauthenticated.hourlyTicker.exampleResponse);
+
+nock(rootUrl)
+    .get('/ticker_hour/btcusd')
+    .twice()
+    .replyWithError(testErrMsg);
+
+nock(rootUrl)
+    .get('/order_book/btcusd')
+    .twice()
+    .reply(200, apis.unauthenticated.orderBook.exampleResponse);
+
+nock(rootUrl)
+    .get('/order_book/btcusd')
+    .twice()
+    .replyWithError(testErrMsg);
+
 
 describe.only('bitstamp', function () {
 
@@ -49,6 +69,76 @@ describe.only('bitstamp', function () {
 
             it('retrieves error using promise', function (done) {
                 bitstamp.ticker({currencyPair: 'btcusd'}).then(
+                    success,
+                    failure(done)
+                );
+            });
+        });
+    });
+
+    describe('hourly ticker', function () {
+
+        context('success call', function () {
+            it('retrieves hourly ticker data using cb', function (done) {
+                bitstamp.hourlyTicker({currencyPair: 'btcusd'}, function (err, resp) {
+                    resp.should.deep.equal(apis.unauthenticated.hourlyTicker.exampleResponse);
+                    done();
+                });
+            });
+
+            it('retrieves hourly ticker using promise', function (done) {
+                bitstamp.hourlyTicker({currencyPair: 'btcusd'}).then(
+                    success(apis.unauthenticated.hourlyTicker.exampleResponse, done),
+                    failure
+                );
+            });
+        });
+
+        context('failure call', function () {
+            it('retrieves error using cb', function (done) {
+                bitstamp.hourlyTicker({currencyPair: 'btcusd'}, function (err, resp) {
+                    err.should.deep.equal(testErrMsg)
+                    done();
+                });
+            });
+
+            it('retrieves error using promise', function (done) {
+                bitstamp.hourlyTicker({currencyPair: 'btcusd'}).then(
+                    success,
+                    failure(done)
+                );
+            });
+        });
+    });
+
+    describe('order book', function () {
+
+        context('success call', function () {
+            it('retrieves order book data using cb', function (done) {
+                bitstamp.orderBook({currencyPair: 'btcusd'}, function (err, resp) {
+                    resp.should.deep.equal(apis.unauthenticated.orderBook.exampleResponse);
+                    done();
+                });
+            });
+
+            it('retrieves order book using promise', function (done) {
+                bitstamp.orderBook({currencyPair: 'btcusd'}).then(
+                    success(apis.unauthenticated.orderBook.exampleResponse, done),
+                    failure
+                );
+            });
+        });
+
+        context('failure call', function () {
+            it('retrieves error using cb', function (done) {
+                bitstamp.orderBook({currencyPair: 'btcusd'}, function (err, resp) {
+                    err.should.deep.equal(testErrMsg)
+                    done();
+                });
+            });
+
+            it('retrieves error using promise', function (done) {
+                bitstamp.orderBook({currencyPair: 'btcusd'}).then(
                     success,
                     failure(done)
                 );
